0a146afa4dd8711c2d46dc41c469f052201b5c69
Syakhisk <mail.syakhisk@gmail.com>
2021-11-06 10:56:24 +0700

    update rofi

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -1,55 +1,62 @@
-local filesystem = require('gears.filesystem')
+local filesystem = require("gears.filesystem")
 local config_dir = filesystem.get_configuration_dir()
-local utils_dir = config_dir .. 'utilities/'
+local utils_dir = config_dir .. "utilities/"
 
 return {
 	-- The default applications that we will use in keybindings and widgets
 	default = {
 		-- Default terminal emulator
-		terminal = 'alacritty',
+		terminal = "alacritty",
 		-- Default web browser
-		web_browser = 'firefox',
+		web_browser = "firefox",
 		-- Default text editor
-		text_editor = 'alacritty -e lvim',
+		text_editor = "alacritty -e lvim",
 		-- Default file manager
-		file_manager = 'thunar',
+		file_manager = "thunar",
 		-- Default media player
-		multimedia = 'mpv',
+		multimedia = "mpv",
 		-- Default game, can be a launcher like steam
-		game = 'supertuxkart',
+		game = "supertuxkart",
 		-- Default graphics editor
-		graphics = 'gimp-2.10',
+		graphics = "gimp-2.10",
 		-- Default sandbox
-		sandbox = 'virtualbox',
+		sandbox = "virtualbox",
 		-- Default IDE
-		development = 'alacritty -e lvim',
+		development = "alacritty -e lvim",
 		-- Default network manager
-		network_manager = 'alacritty -e nmtui',
+		network_manager = "alacritty -e nmtui",
 		-- Default bluetooth manager
-		bluetooth_manager = 'blueman-manager',
+		bluetooth_manager = "blueman-manager",
 		-- Default power manager
-		power_manager = 'xfce4-power-manager',
+		power_manager = "xfce4-power-manager",
 		-- Default GUI package manager
-		package_manager = 'pamac-manager',
+		package_manager = "pamac-manager",
 		-- Default locker
-		lock = 'awesome-client "awesome.emit_signal(\'module::lockscreen_show\')"',
+		lock = "awesome-client \"awesome.emit_signal('module::lockscreen_show')\"",
 		-- Default quake terminal
-		quake = 'kitty --name QuakeTerminal',
+		quake = "kitty --name QuakeTerminal",
 		-- Default rofi global menu
-		rofi_global = 'rofi -dpi ' .. screen.primary.dpi .. 
-							' -show "Global Search" -modi "Global Search":' .. config_dir .. 
-							'/configuration/rofi/global/rofi-spotlight.sh' .. 
-							' -theme ' .. config_dir ..
-							'/configuration/rofi/global/rofi.rasi',
-		rofi_window = 'rofi -dpi ' .. screen.primary.dpi .. 
-							' -show window -modi "Window":' .. config_dir .. 
-							'/configuration/rofi/global/rofi-spotlight.sh' .. 
-							' -theme ' .. config_dir ..
-							'/configuration/rofi/global/rofi.rasi',
+		rofi_global = "rofi -dpi "
+			.. screen.primary.dpi
+			.. ' -show "Global Search" -modi "Global Search":'
+			.. config_dir
+			.. "/configuration/rofi/global/rofi-spotlight.sh"
+			.. " -theme "
+			.. config_dir
+			.. "/configuration/rofi/global/rofi.rasi",
+		rofi_window = "rofi -dpi "
+			.. screen.primary.dpi
+			.. " -show combi"
+			.. " -combi-modi Brotab:rofi-brotab,window"
+			.. " -theme "
+			.. config_dir
+			.. "/configuration/rofi/global/rofi.rasi",
 		-- Default app menu
-		rofi_appmenu = 'rofi -dpi ' .. screen.primary.dpi ..
-							' -show drun -theme ' .. config_dir ..
-							'/configuration/rofi/appmenu/rofi.rasi'
+		rofi_appmenu = "rofi -dpi "
+			.. screen.primary.dpi
+			.. " -show drun -theme "
+			.. config_dir
+			.. "/configuration/rofi/appmenu/rofi.rasi",
 
 		-- You can add more default applications here
 	},
@@ -57,39 +64,38 @@ return {
 	-- List of apps to start once on start-up
 	run_on_start_up = {
 		-- Compositor
-		'picom -b --experimental-backends --dbus --config ' ..
-		config_dir .. '/configuration/picom.conf',
+		"picom -b --experimental-backends --dbus --config " .. config_dir .. "/configuration/picom.conf",
 		-- Blueman applet
-		'blueman-applet',
+		"blueman-applet",
 		-- Music server
 		-- 'mpd',
 		-- Polkit and keyring
-		'/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1',
+		"/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1",
 		-- '/usr/bin/lxqt-policykit-agent &' ..
 		-- ' eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)',
 		-- Load X colors
 		-- 'xrdb $HOME/.Xresources',
 		-- Audio equalizer
-		'pulseeffects --gapplication-service',
+		"pulseeffects --gapplication-service",
 		-- Lockscreen timer
-		[[
-		xidlehook --not-when-fullscreen --not-when-audio --timer 600 \
-		"awesome-client 'awesome.emit_signal(\"module::lockscreen_show\")'" ""
-		]],
+		-- [[
+		-- xidlehook --not-when-fullscreen --not-when-audio --timer 600 \
+		-- "awesome-client 'awesome.emit_signal(\"module::lockscreen_show\")'" ""
+		-- ]],
 
 		-- You can add more start-up applications here
-		'remaps',
-		'switchdisplay-quiet',
-		'pulseaudio',
+		"remaps",
+		"switchdisplay-quiet",
+		"pulseaudio",
 	},
 
 	-- List of binaries/shell scripts that will execute for a certain task
 	utils = {
 		-- Fullscreen screenshot
-		full_screenshot = utils_dir .. 'snap full',
+		full_screenshot = utils_dir .. "snap full",
 		-- Area screenshot
-		area_screenshot = utils_dir .. 'snap area',
+		area_screenshot = utils_dir .. "snap area",
 		-- Update profile picture
-		update_profile  = utils_dir .. 'profile-image'
-	}
+		update_profile = utils_dir .. "profile-image",
+	},
 }
